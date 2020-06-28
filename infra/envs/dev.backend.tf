4fcb699910b0025bb1eafb4c93f3de1404a1905a
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-28 15:45:59 -0300

    fix: backend dev config #10

--- infra/envs/dev.backend.tf
+++ infra/envs/dev.backend.tf
@@ -1,13 +1,9 @@
-terraform {
-  required_version = ">= 0.12.2"
-
-  backend "s3" {
-    region         = "us-east-1"
-    bucket         = "happ-deniojunior-dev-terraform-state"
-    key            = "terraform.tfstate"
-    dynamodb_table = "happ-deniojunior-dev-terraform-state-lock"
-    profile        = ""
-    role_arn       = ""
-    encrypt        = "true"
-  }
+backend "s3" {
+  region         = "us-east-1"
+  bucket         = "happ-deniojunior-dev-terraform-state"
+  key            = "terraform.tfstate"
+  dynamodb_table = "happ-deniojunior-dev-terraform-state-lock"
+  profile        = ""
+  role_arn       = ""
+  encrypt        = "true"
 }
