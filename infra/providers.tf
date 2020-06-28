24210a1ee94b000348a4c033a8c6066529856a08
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-28 11:44:53 -0300

    feat: add namespace config #6

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,5 +2,3 @@ provider "aws" {
   version = "~> 2.0"
   region  = var.aws_region
 }
-
-data "aws_region" "current" {}
--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,14 @@ variable "app" {
   type        = string
   description = "Application name"
 }
+
+variable "namespace" {
+  type        = string
+  description = "Application namespace to specify context and avoid bucket name conflicts"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region where the infrastructure is located"
+  default     = "us-east-1"
+}
