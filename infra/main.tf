a0c54a829b942452e779cd25e67ccfcf230087ce
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-28 11:02:45 -0300

    feat: importing state module and create frontend s3 bucket #6

--- infra/main.tf
+++ infra/main.tf
@@ -2,24 +2,29 @@ locals {
   resource = "${var.app}.${var.env}"
 }
 
+module "terraform_state_backend" {
+  source                             = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=master"
+  namespace                          = "happ"
+  delimiter                          = "."
+  stage                              = var.env
+  name                               = "terraform"
+  attributes                         = ["state"]
+  region                             = "us-east-1"
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
+}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket_name
-  acl    = "private"
+  bucket        = "${local.resource}.frontend"
+  acl           = "private"
   force_destroy = true
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     Application = var.app
     Environment = var.env
-    Name        = var.bucket_name
+    Name        = "${local.resource}.frontend"
   }
 }
