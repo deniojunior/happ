84ce318d83d6d3d6bf397d676df904ca23811902
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-28 17:22:26 -0300

    feat: wip backend ci workflow #10

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
  output "ecr_repository" {
-  value       = aws_ecr_repository..ecr_repository.name
+  value       = aws_ecr_repository.ecr_repository.name
   description = "ECR repository name"
 }
 
 output "ecr_repository_url" {
-  value       = aws_ecr_repository..ecr_repository.repository_url
+  value       = aws_ecr_repository.ecr_repository.repository_url
   description = "ECR repository URL"
 }
