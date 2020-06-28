9392272a13f89b87396b806da8bb3f5c21f9d28c
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-28 18:56:00 -0300

    feat: wip fronend workdflow #10

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,8 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.ecr_repository.repository_url
   description = "ECR repository URL"
 }
+
+output "frontend_bucket" {
+  value       = module.s3_bucket.this_s3_bucket_id
+  description = "Frontend s3 bucket"
+}
