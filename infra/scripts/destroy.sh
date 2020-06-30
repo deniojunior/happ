012bbc1f03a9984cef1a28345d8590d111c47e28
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-30 12:58:13 -0300

    dix: destroy script #15

--- infra/scripts/destroy.sh
+++ infra/scripts/destroy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-#mkdir tmp
+mkdir tmp
 
-terraform init -force-copy
-rm -rf .terraform
-
-echo -e "terraform { \nbackend \"local\" {}\n }" > /tmp/backend.tf
-yes -yes | terraform init -backend-config=/tmp/backend.tf
+terraform init -force
 
 echo "$(terraform state list | grep -vE module.cloufront_multiorigin.aws_lambda_function.lambda_edge)" > tmp/resources_to_destroy
 echo "terraform destroy -auto-approve $@ \\" > tmp/destroy_script.sh
@@ -16,5 +12,7 @@ while read resource; do
 done < tmp/resources_to_destroy
 
 sed -i '$ s/.$//' tmp/destroy_script.sh
+
 bash tmp/destroy_script.sh;
+
 rm -rf tmp
