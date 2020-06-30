332b3dcb76e490ba4777112bc4215a20296eeb06
deniojunior <deniofriacamoreirajr@gmail.com>
2020-06-30 03:47:11 -0300

    fix: lambda edge edge #15

--- infra/modules/cloudfront_multiorigin/resources/lambda_edge_function.js
+++ infra/modules/cloudfront_multiorigin/resources/lambda_edge_function.js
@@ -4,7 +4,7 @@ module.exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
   
   // Remove context before origin connection
-  request.uri = request.uri.replace(/^\/frontend/, "");
+  request.uri = request.uri.replace(/^\/frontend/, "/index.html");
   request.uri = request.uri.replace(/^\/backend/, "");
   
   callback(null, request);
