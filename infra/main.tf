7d0c46ade227d3069b6ccbae72ea90e22f034150
deniojunior <deniofriacamoreirajr@gmail.com>
2020-07-01 04:21:22 -0300

    fix: k8s deploy #14

--- infra/main.tf
+++ infra/main.tf
@@ -103,18 +103,18 @@ module "vpc" {
   name = "${local.resource}-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  
+  azs = ["${var.aws_region}a", "${var.aws_region}b"]
+
   private_subnets = var.vpc_private_subnets
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.resource}-eks" = "shared"
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 
-  public_subnets  = var.vpc_public_subnets
+  public_subnets = var.vpc_public_subnets
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.resource}-eks" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 
   enable_nat_gateway = true
@@ -132,6 +132,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
+  alias                  = "eks"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
@@ -140,7 +141,6 @@ provider "kubernetes" {
 }
 
 module "eks" {
-  alias = "eks"
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "${local.resource}-eks"
   cluster_version = "1.16"
