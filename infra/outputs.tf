af0de3197deeeed8e2ef1de96783d7110153f554
deniojunior <deniofriacamoreirajr@gmail.com>
2020-07-01 06:33:01 -0300

    feat: acm certificate arn #15

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,8 @@ output "frontend_bucket" {
   value       = module.s3_bucket.this_s3_bucket_id
   description = "Frontend s3 bucket"
 }
+
+output "acm_certificate_arn" {
+  value       = module.acm.this_acm_certificate_arn
+  description = "ACM certificace arn"
+}
